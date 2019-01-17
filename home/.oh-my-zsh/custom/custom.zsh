a624bed9688f9306285c4aa1aafce3a3c73d897c
bremme <bramharmsen@gmail.com>
2019-01-17 22:57:26 +0100

    Add custom path functions

--- home/.oh-my-zsh/custom/custom.zsh
+++ home/.oh-my-zsh/custom/custom.zsh
@@ -3,3 +3,15 @@ if [ -n "$DESKTOP_SESSION" ];then
     eval $(gnome-keyring-daemon --start)
     export SSH_AUTH_SOCK
 fi
+
+
+# add to path if not already done so
+addpath() {
+  if [[ ":$PATH:" != *":$1:"* ]] ; then
+    PATH=$1:$PATH
+  fi
+}
+
+showpath() {
+  echo $PATH
+}
