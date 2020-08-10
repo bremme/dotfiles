281a9bb5c96191fda0695fdf1582b2f2405cd94e
bremme <bramharmsen@gmail.com>
2020-08-10 15:42:33 +0200

    Added custom gsettings scripts

---
+++ home/scripts/custom-gsettings.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Set right hand tabbing
+gsettings\
+  set org.gnome.desktop.wm.keybindings switch-applications\
+  "['<Super>Tab', '<Alt>Tab', '<Ctrl>backslash']"
+# Set right hand tabbing backwards
+gsettings\
+  set org.gnome.desktop.wm.keybindings switch-applications-backward\
+  "['<Shift><Super>Tab', '<Shift><Alt>Tab', '<Shift><Ctrl>backslash']"
+# Set right hand group tabbing
+gsettings\
+  set org.gnome.desktop.wm.keybindings  switch-group\
+  "['<Super>Above_Tab', '<Alt>Above_Tab', '<Ctrl>backspace']"
+# Set right hand group tabbing backwards
+gsettings\
+  set org.gnome.desktop.wm.keybindings  switch-group-backward\
+  "['<Shift><Super>Above_Tab', '<Shift><Alt>Above_Tab', '<Shift><Ctrl>backspace']"
