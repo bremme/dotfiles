c248e8e50b36a760aa4d77cf66cefc42e662b316
Bram Harmsen <bramharmsen@gmail.com>
2021-06-28 10:59:12 +0200

    Add script to instal a different filemanager

---
+++ scripts/set_filemanager.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+source $(dirname "$0")/utils.sh
+
+function install_nemo() {
+    local name="Nemo (deb)"
+    log_info "Install '$name'"
+    sudo apt install nemo
+
+    # Set nemo as default application
+    xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
+    # disable nautilus from displaying desktop icons
+    gsettings set org.gnome.desktop.background show-desktop-icons false
+    gsettings set org.nemo.desktop show-desktop-icons true
+}
+
+function install_nautilus() {
+    local name="Nautilus (deb)"
+    log_info "Install '$name'"
+    sudo apt install nautilus
+
+    # Set naulitus as default application
+    xdg-mime default nautilus.desktop inode/directory application/x-gnome-saved-search
+    # disable nautilus from displaying desktop icons
+    gsettings set org.gnome.desktop.background show-desktop-icons true
+        gsettings set org.nemo.desktop show-desktop-icons false
+}
+
+echo "Which filemanager do you want to use?"
+echo ""
+echo "1. nautilus"
+echo "2. nemo "
+echo ""
+number=$(choose_number)
+
+if [ "$number" -eq 1 ]; then
+    install_nautilus
+elif [ "$number" -eq 2 ]; then
+    install_nemo
+else
+    log_info "Unknown filemanager '$number'"
+fi
