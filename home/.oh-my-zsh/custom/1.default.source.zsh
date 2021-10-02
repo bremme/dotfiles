dea0d1a76e11f91119d4535a7a66978adba56c2c
Bram Harmsen <bram.harmsen@alliander.com>
2021-10-02 20:25:32 +0200

    Source kubectl and oc completions when available

--- home/.oh-my-zsh/custom/1.default.source.zsh
+++ home/.oh-my-zsh/custom/1.default.source.zsh
@@ -2,3 +2,12 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 # load zsh_command_not_found if present
 [ -f /etc/zsh_command_not_found ] && source /etc/zsh_command_not_found
+
+
+if command -v oc > /dev/null 2>&1; then
+    source <(oc completion zsh)
+fi
+
+if command -v kubectl > /dev/null 2>&1; then
+    source <(kubectl completion zsh)
+fi
