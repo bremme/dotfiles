b100980164931b5b66944aba2175068d4e29f93c
Bram Harmsen <bram.harmsen@alliander.com>
2022-05-13 09:21:52 +0200

    Fix typo

--- home/.oh-my-zsh/custom/2.apps.java.zsh
+++ home/.oh-my-zsh/custom/2.apps.java.zsh
@@ -1,5 +1,5 @@
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-alias spa = "gradle spotlessApply"
-alias spag = "gradle spotlessApply && git add --update"
+alias spa="gradle spotlessApply"
+alias spag="gradle spotlessApply && git add --update"
