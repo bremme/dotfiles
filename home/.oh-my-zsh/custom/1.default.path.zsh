b189f8a93516ca97bcab6da91dd089d915dc5eb3
Bram Harmsen <6715707+bremme@users.noreply.github.com>
2023-08-09 21:17:14 +0200

    Fix typo

--- home/.oh-my-zsh/custom/1.default.path.zsh
+++ home/.oh-my-zsh/custom/1.default.path.zsh
@@ -4,6 +4,4 @@ PATH=$HOME/.local/bin:$HOME/bin:$HOME/bin/openapitools:/sbin:$PATH
 # Add Homebrew to PATH
 if [[ -x "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-else
-
-
+fi
