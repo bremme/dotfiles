979042e5b98f3c5de97d7813fc639e90787b2d24
Bram Harmsen <6715707+bremme@users.noreply.github.com>
2023-08-15 21:58:56 +0200

    fix run local script

--- run-local.sh
+++ run-local.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+echo "ansible-playbook
+    --inventory inventory.ini
+    --limit "$(hostname -f)"
+    --ask-become-pass
+    --connection local
+    -vvv
+    "$*"
+    local.yml"
+
 ansible-playbook \
     --inventory inventory.ini \
     --limit "$(hostname -f)" \
     --ask-become-pass \
     --connection local \
-    -vvv \
-    "$*" \
+    --verbose \
+    $* \
     local.yml
