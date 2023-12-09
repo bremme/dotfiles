3d53b350c83f7418819e2950e73da7fecf23126c
Bram Harmsen <6715707+bremme@users.noreply.github.com>
2023-12-09 12:13:44 +0100

    Fix typo

--- home/.oh-my-zsh/custom/2.apps.node.zsh
+++ home/.oh-my-zsh/custom/2.apps.node.zsh
@@ -1,9 +1,9 @@
 # Setup NVM
-if [-d "$HOME/.nvm" ]; then
+if [ -d "$HOME/.nvm" ] ; then
   export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-if
+fi
 
 # FNM (faster alternative to NVM)
 if [ -d "$HOME/.local/share/fnm" ] ; then
