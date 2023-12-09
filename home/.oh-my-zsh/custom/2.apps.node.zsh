8da02e158069897ec3f92157a399ef020da2c7ef
Bram Harmsen <6715707+bremme@users.noreply.github.com>
2023-12-09 12:05:36 +0100

    Remove lazy loading

--- home/.oh-my-zsh/custom/2.apps.node.zsh
+++ home/.oh-my-zsh/custom/2.apps.node.zsh
@@ -1,38 +1,43 @@
-# Only set lazy loading aliases if nvm is installed
-if [ -d "$HOME/.nvm" ]; then
+# # Only set lazy loading aliases if nvm is installed
+# if [ -d "$HOME/.nvm" ]; then
 
-  # lazy load nvm, will trigger on first use of nvm, node or npm
-  load_nvm() {
-    unset -f nvm
-    unset -f node
-    unset -f npm
-    unset -f load_nvm
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                    # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-  }
+#   # lazy load nvm, will trigger on first use of nvm, node or npm
+#   load_nvm() {
+#     unset -f nvm
+#     unset -f node
+#     unset -f npm
+#     unset -f load_nvm
+#     export NVM_DIR="$HOME/.nvm"
+#     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                    # This loads nvm
+#     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-  nvm() {
-    load_nvm
-    nvm "$@"
-  }
+#   }
 
-  node() {
-    load_nvm
-    node "$@"
-  }
+#   nvm() {
+#     load_nvm
+#     nvm "$@"
+#   }
 
-  npm() {
-    load_nvm
-    npm "$@"
-  }
+#   node() {
+#     load_nvm
+#     node "$@"
+#   }
 
-  uninstall_nvm() {
-    rm -r $NVM_DIR
-    unset -f nvm
-    unset -f node
-    unset -f npm
-    unset -f load_nvm
-    unset NVM_DIR
-  }
-fi
+#   npm() {
+#     load_nvm
+#     npm "$@"
+#   }
+
+#   uninstall_nvm() {
+#     rm -r $NVM_DIR
+#     unset -f nvm
+#     unset -f node
+#     unset -f npm
+#     unset -f load_nvm
+#     unset NVM_DIR
+#   }
+# fi
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
