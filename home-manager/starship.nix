8e5d014c367f63b387248be0389db1bd0befc307
bremme <6715707+bremme@users.noreply.github.com>
2024-10-19 15:05:41 +0200

    fix git commit commands

--- home-manager/starship.nix
+++ home-manager/starship.nix
@@ -5,7 +5,16 @@
       enable = true;
       enableZshIntegration = true;
       settings = {
-        add_newline = false;  # Add a new line before the start of the prompt.
+        # Add a new line before the start of the prompt.
+        add_newline = false;  
+      
+        username = {
+          style_user = "white bold";
+          style_root = "red bold";
+          format = "[$user]($style)";
+          disabled = false;
+          show_always = true;
+        };
       };
     };
 }
