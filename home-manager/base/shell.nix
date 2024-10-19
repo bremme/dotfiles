ec6446b94babecef527f1dc9988b8665c993406e
bremme <6715707+bremme@users.noreply.github.com>
2024-10-19 23:02:25 +0200

    Move zsh config to shell config

--- home-manager/base/shell.nix
+++ home-manager/base/shell.nix
@@ -4,4 +4,33 @@
   imports = [
     ./starship/starship.nix
   ];
+
+  programs = {
+    zsh = {
+      enable = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ 
+          # "git" 
+        ];
+        theme = "robbyrussell";
+        custom = "$HOME/dotfiles/dotfiles/oh-my-zsh";
+      };
+      autosuggestion = {
+        enable = true;
+      };
+    };
+
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      options = [ "--cmd" "cd" ];
+    };
+
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    }
+
+  };
 }
