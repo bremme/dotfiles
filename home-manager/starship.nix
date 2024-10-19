26c7af9973d3df00e78e32b971396b0616991f57
bremme <6715707+bremme@users.noreply.github.com>
2024-10-19 11:54:31 +0200

    Move starship config

---
+++ home-manager/starship.nix
@@ -0,0 +1,11 @@
+{ lib, ... }:
+
+{
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+        add_newline = false;  # Add a new line before the start of the prompt.
+      };
+    };
+}
