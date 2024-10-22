95076558b90df91a734476283f59c2bfb61aa23f
bremme <6715707+bremme@users.noreply.github.com>
2024-10-22 21:35:59 +0200

    Install important gnome extensions

--- home-manager/workstation/gnome/extensions.nix
+++ home-manager/workstation/gnome/extensions.nix
@@ -1,5 +1,27 @@
-{ ... }:
+{ pkgs, lib, ... }:
+
+with lib.hm.gvariant;
 
 {
-  
+  programs.gnome-shell = with pkgs.gnomeExtensions; {
+    enable = true;
+    extensions = [
+      {
+        id = "caffeine@patapon.info";
+        package = caffeine;
+      }
+      {
+        id = "Vitals@CoreCoding.com";
+        package = vitals;
+      }
+    ];
+  };
+
+  # Extensions settings
+  dconf.settings = {
+
+    "org/gnome/shell/extensions/vitals" = {
+      icon-style = mkInt32 1;
+    };
+  };
 }
